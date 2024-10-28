73e18c26aa3e846638cacdd0ff33ff1089dec8c4
Altynbek2021 <shatmanalie0694@gmail.com>
2024-10-28 23:47:07 +0600

    edited appbar

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -7,14 +7,31 @@ class MainPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Hello"),
+        backgroundColor: Colors.greenAccent,
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.menu),
+          iconSize: 35,
+          color: const Color.fromARGB(255, 136, 21, 139),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.sort),
+            iconSize: 35,
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.search),
+            iconSize: 35,
+          )
+        ],
+        title: Text("Нарын Базар"),
+        centerTitle: true,
       ),
       body: Container(
         child: Column(
-          children: [
-            Text("Нарын Базар"),
-            Text("You need to do app bar and navigation bar")
-          ],
+          children: [Text("You need to do app bar and navigation bar")],
         ),
       ),
     );
