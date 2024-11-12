fdb9495dcb2cc6569b60c35bc30166019532a49c
Altynbek2021 <shatmanalie0694@gmail.com>
2024-11-12 03:45:40 -0500

    added input field for phone and what's app field

--- lib/pages/news_insert_page/news_insert_page.dart
+++ lib/pages/news_insert_page/news_insert_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:naryn_market/constants/colors.dart';
 import 'package:naryn_market/pages/categories_page/categories_page.dart';
+import 'package:naryn_market/constants/text_styles/text_styles.dart';
+import 'package:intl_phone_field/intl_phone_field.dart';
 
 class NewsInsertPage extends StatefulWidget {
   NewsInsertPage({super.key});
@@ -25,8 +27,8 @@ class _NewsInsertPageState extends State<NewsInsertPage> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Text("Суроттор"),
-          Text("Жарыяныздын суроту болсо. томонго жуктонуз"),
+          Text("Суроттор", style: TextStyles.description),
+          Text("Жарыяныздын суроту болсо, томонго жуктонуз"),
           StaggeredGrid.count(
             crossAxisCount: 4,
             mainAxisSpacing: 4,
@@ -74,21 +76,28 @@ class _NewsInsertPageState extends State<NewsInsertPage> {
               ),
             ], // End off grid
           ),
-          Text("Муноздомо"),
+          Text(
+            "Муноздомо",
+            style: TextStyles.description,
+          ),
           TextField(
             maxLength: 300,
             onChanged: (context) {},
             maxLines: 4,
             decoration: InputDecoration(hintText: "Бул жерге жазыныз"),
           ),
-          Text("Категориялар"),
+          Text(
+            "Категориялар",
+            style: TextStyles.description,
+          ),
           Container(
             height: 50,
-            width: 300,
-            color: AppColors.blue20,
+            width: 400,
+            color: AppColors.starGrey,
             child: DropdownButton<String>(
               value: selectedCategory,
-              hint: Text("Choose a category"),
+              hint: Text("Категория танданыз"),
+              borderRadius: BorderRadius.circular(40),
               items: categoryTitles.map((String title) {
                 return DropdownMenuItem<String>(
                   value: title,
@@ -102,7 +111,35 @@ class _NewsInsertPageState extends State<NewsInsertPage> {
               },
             ),
           ),
-          Text("Валюта"),
+          Text(
+            "Телефон номеринизди жазыныз",
+            style: TextStyles.description,
+          ),
+          IntlPhoneField(
+            decoration: InputDecoration(
+              labelText: 'Телефон номер',
+              border: OutlineInputBorder(
+                borderSide: BorderSide(),
+              ),
+            ),
+            initialCountryCode: 'KG',
+            onChanged: (phone) {
+              print(phone.completeNumber);
+            },
+          ),
+          Text("What's app номер жазыныз", style: TextStyles.description),
+          IntlPhoneField(
+            decoration: InputDecoration(
+              labelText: 'What\'s app номер жвзыныз',
+              border: OutlineInputBorder(
+                borderSide: BorderSide(),
+              ),
+            ),
+            initialCountryCode: 'KG',
+            onChanged: (phone) {
+              print(phone.completeNumber);
+            },
+          ),
         ],
       ),
     );
